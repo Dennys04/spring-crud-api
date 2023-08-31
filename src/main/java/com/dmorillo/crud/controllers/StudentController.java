27ce53ace881d493635da3a3de286d0300cf600d
Dennys Morillo <67664070+Dennys04@users.noreply.github.com>
2023-08-31 22:11:14 +0200

    Update StudentController.java

--- src/main/java/com/dmorillo/crud/controllers/StudentController.java
+++ src/main/java/com/dmorillo/crud/controllers/StudentController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/students")
+@RequestMapping("/api/v1/student/")
 public class StudentController {
     private final StudentService studentService;
 
